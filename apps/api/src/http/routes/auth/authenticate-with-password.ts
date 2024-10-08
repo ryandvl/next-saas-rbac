74aeb043e0d1a490f53fcbb3dd983829dc80d52e
ryandvl <contatoryandvl55@gmail.com>
2024-10-08 20:08:41 -0300

    feat(api): profile user logged route

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -15,6 +15,14 @@ export async function authenticateWithPassword(app: FastifyInstance) {
           email: z.string().email(),
           password: z.string().min(6),
         }),
+        response: {
+          400: z.object({
+            message: z.string(),
+          }),
+          201: z.object({
+            token: z.string(),
+          }),
+        },
       },
     },
     async (request, reply) => {
