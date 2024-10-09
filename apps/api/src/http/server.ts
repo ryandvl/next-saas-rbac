345cf2ccadaef0d2f66322704c0cbddaf137b81f
ryandvl <contatoryandvl55@gmail.com>
2024-10-09 16:29:25 -0300

    chore(api): setup 0.0.0.0 as default api host

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -117,6 +117,6 @@ app.register(revokeInvite)
 // billing
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
