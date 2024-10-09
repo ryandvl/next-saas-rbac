239fa673929ce166cf3f5c14708bfd94a3150af0
ryandvl <contatoryandvl55@gmail.com>
2024-10-09 01:11:17 -0300

    feat(web): form error states

---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { AlertTriangle, Loader2 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useActionState } from 'react'
+
+import githubIcon from '@/assets/github-icon.svg'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+import { signInWithEmailAndPassword } from './actions'
+
+export function SignInForm() {
+  const [{ errors, message, success }, formAction, isPending] = useActionState(
+    signInWithEmailAndPassword,
+    { success: false, message: null, errors: null }
+  )
+
+  return (
+    <form action={formAction} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
+
+        <Link
+          href="/auth/forgot-password"
+          className="text-xs font-medium text-foreground hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+
+      <Button className="w-full" type="submit" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size-4 animate-spin" />
+        ) : (
+          'Sign in with e-mail'
+        )}
+      </Button>
+
+      <Button className="w-full" variant="link" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
+      <Separator />
+
+      <Button type="submit" className="w-full" variant="outline">
+        <Image src={githubIcon} alt="" className="mr-2 size-4 dark:invert" />
+        Sign in with GitHub
+      </Button>
+    </form>
+  )
+}
--- apps/web/src/components/ui/alert.tsx
+++ apps/web/src/components/ui/alert.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
@@ -10,7 +10,7 @@ const alertVariants = cva(
       variant: {
         default: 'bg-background text-foreground',
         destructive:
-          'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+          'border-red-500 text-red-500 [&>svg]:text-red-500 dark:text-red-400 dark:border-red-400 dark:[&>svg]:text-red-400',
       },
     },
     defaultVariants: {
