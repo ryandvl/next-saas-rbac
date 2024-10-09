c445dc0a0d57656cd7159890b787cc8e5d4842c8
ryandvl <contatoryandvl55@gmail.com>
2024-10-09 14:51:38 -0300

    feat(web): project creation modal

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,25 +1,19 @@
-import { redirect } from 'next/navigation'
+import { ProjectForm } from '@/app/(app)/org/[slug]/create-project/project-form'
+import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
+import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 
-import { ability } from '@/auth/auth'
-import { Header } from '@/components/header'
-
-import { ProjectForm } from './project-form'
-
-export default async function CreateProject() {
-  const permissions = await ability()
-
-  if (permissions?.cannot('create', 'Project')) {
-    redirect('/')
-  }
-
+export default function CreateProject() {
   return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create project</h1>
+    <Sheet defaultOpen>
+      <InterceptedSheetContent>
+        <SheetHeader>
+          <SheetTitle>Create project</SheetTitle>
+        </SheetHeader>
 
-        <ProjectForm />
-      </main>
-    </div>
+        <div className="py-4">
+          <ProjectForm />
+        </div>
+      </InterceptedSheetContent>
+    </Sheet>
   )
 }
