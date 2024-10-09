ec7e4464fd69c065370f16d3c62c91d84720d89e
ryandvl <contatoryandvl55@gmail.com>
2024-10-09 02:30:28 -0300

    feat(web): sheet

---
+++ apps/web/src/app/(app)/create-organization/layout.tsx
@@ -0,0 +1,22 @@
+import { redirect } from 'next/navigation'
+
+import { isAuthenticated } from '@/auth/auth'
+
+export default function AppLayout({
+  children,
+  sheet,
+}: Readonly<{
+  children: React.ReactNode
+  sheet: React.ReactNode
+}>) {
+  if (!isAuthenticated()) {
+    redirect('/auth/sign-in')
+  }
+
+  return (
+    <>
+      {children}
+      {sheet}
+    </>
+  )
+}
