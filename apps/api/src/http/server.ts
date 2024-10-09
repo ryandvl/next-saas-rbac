48172d2ceb20fd39ef0c7d8387ac22e824ed4aa8
ryandvl <contatoryandvl55@gmail.com>
2024-10-08 21:40:35 -0300

    feat(api): create password change auth route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -14,6 +14,8 @@ import { authenticateWithPassword } from './routes/auth/authenticate-with-passwo
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { errorHandler } from './error-handler'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -54,6 +56,8 @@ app.register(fastifyCors)
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getProfile)
+app.register(requestPasswordRecover)
+app.register(resetPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
